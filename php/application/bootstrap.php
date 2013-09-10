01002b05fa6c1a0f396e10d6f805c51a4cce51a5
marcelopacheco <marcelopacheco@192.168.0.15>
2013-09-09 21:27:06 -0300

    Ajuste de caminho PHP
    09/09/13 - 21:26

--- php/application/bootstrap.php
+++ php/application/bootstrap.php
@@ -103,7 +103,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(array(
-	'base_url'   => '/',
+	'base_url'   => '/PosMackenzie/php/',
 ));
 
 //Cookie salt
