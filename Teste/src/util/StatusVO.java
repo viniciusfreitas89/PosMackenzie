3353671c983c61f6de41f67c08ee6d3028dd2b9c
Vinicius Freitas <viniciushf@live.com>
2013-09-15 20:14:07 -0300

    Aplicação para testes

---
+++ Teste/src/util/StatusVO.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.util.List;
+
+/**
+ * Objeto que representa o resultado JSON retornado pelo serviço de Login em PHP.
+ * A URL de acesso para este serviço esta em: Constants.SERVICES_URL_LOGIN
+ * @author Vinicius
+ * 
+ */
+public class StatusVO {
+	private boolean status;
+	private String msg;
+	private UsuarioVO usuario;
+	private List<CategoriaVO> categorias;
+	
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public UsuarioVO getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(UsuarioVO usuario) {
+		this.usuario = usuario;
+	}
+	public List<CategoriaVO> getCategorias() {
+		return categorias;
+	}
+	public void setCategorias(List<CategoriaVO> categorias) {
+		this.categorias = categorias;
+	}
+}
---
+++ Teste/src/util/WebUtils.java
@@ -0,0 +1,151 @@
+package util;
+
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * 
+ * @author Vinicius
+ *
+ */
+public class WebUtils {
+	
+	/**
+	 * Recomendado para conexões sem proxy. 
+	 * @param url : String
+	 * @param params : List<NameValuePair>
+	 * @return InputStream
+	 * @throws Exception
+	 */
+	public static InputStream requestByPost(String url, List<NameValuePair> params) throws Exception{
+		
+		return request(url, params, null, null, null, null, true);
+	}
+	
+	/**
+	 * Recomendado para conexões com proxy. 
+	 * @param url : String
+	 * @param params : List<NameValuePair>
+	 * @return InputStream
+	 * @throws Exception
+	 */
+	public static InputStream requestByPost(String url,
+											List<NameValuePair> params, 
+											String host, 
+											Integer porta, 
+											String login, 
+											String senha) throws Exception{
+		
+		return request(url, params, host, porta, login, senha, true); 
+    }
+	
+	public static InputStream requestByGet(String url, List<NameValuePair> params) throws Exception{
+		
+		return request(url, params, null, null, null, null, false);
+	}
+	
+	/**
+	 * Recomendado para conexões com proxy. 
+	 * @param url : String
+	 * @param params : List<NameValuePair>
+	 * @return InputStream
+	 * @throws Exception
+	 */
+	public static InputStream requestByGet(String url,
+											List<NameValuePair> params, 
+											String host, 
+											Integer porta, 
+											String login, 
+											String senha) throws Exception{
+    	
+    	return request(url, params, host, porta, login, senha, false); 
+    }
+	
+	private static InputStream request(String url,
+										List<NameValuePair> params, 
+										String host, 
+										Integer porta, 
+										String login, 
+										String senha,
+										boolean isPost) throws Exception{
+    	InputStream is = null;
+    	DefaultHttpClient httpclient = new DefaultHttpClient();
+    	
+		try{
+    	     if (host!=null && !host.isEmpty()){
+	    		 if (login!=null && !login.isEmpty() && senha!=null && !senha.isEmpty()){
+	    	    	 httpclient.getCredentialsProvider().setCredentials(
+	 	                    new AuthScope(host, porta),
+	 	                    new UsernamePasswordCredentials(login, senha));
+    	    	 }
+	    		 HttpResponse response;
+	    		 HttpEntity entity;
+	    		 
+	    		 HttpHost proxy = new HttpHost(host, porta);
+    	    	 httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+    	    	 
+    	    	 HttpHost targetHost = new HttpHost(url.replace("http://", ""));
+	             
+	             if (isPost){
+	            	 HttpPost httppost = new HttpPost("/");
+		             httppost.setHeader("content-type", "application/x-www-form-urlencoded");
+		             if (params!=null){
+		            	 httppost.setEntity(new UrlEncodedFormEntity(params));
+		             }
+		             response = httpclient.execute(targetHost, httppost);
+	             }else{
+	            	 HttpGet httpget = new HttpGet("/");
+	            	 httpget.setHeader("content-type", "application/x-www-form-urlencoded");
+		             response = httpclient.execute(targetHost, httpget);
+	             }
+	             
+	             entity = response.getEntity();
+	             if (response.getStatusLine().toString().contains("Proxy Authentication Required")){
+	            	throw new Exception("Autenticação de proxy necessária");
+	             }
+	            
+	             is = entity.getContent();
+    	     }else{
+    	    	 HttpResponse response;
+        	     
+        	     if (isPost){
+        	    	 HttpPost httppost = new HttpPost(url);
+            	     httppost.setHeader("content-type", "application/x-www-form-urlencoded");
+            	     
+            	     if (params!=null){
+            	    	 httppost.setEntity(new UrlEncodedFormEntity(params));
+            	     }
+            	     
+            	     response = httpclient.execute(httppost);
+	             }else{
+		             HttpGet httpget = new HttpGet(url);
+		             httpget.setHeader("content-type", "application/x-www-form-urlencoded");
+            	     
+            	     response = httpclient.execute(httpget);
+	             }
+        	     
+        	     HttpEntity entity = response.getEntity();
+        	     is = entity.getContent();
+    	     }
+	     }catch(Exception e){
+	    	 	e.printStackTrace();
+    	         return null;
+	     }
+		
+    	return is; 
+    }
+}
