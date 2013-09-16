533982025fce6205a7a22a80bc6ef8a13603d017
vinicius.freitas <viniciusfreitas89@hotmail.com>
2013-09-16 13:51:09 -0300

    Teste adicionado

---
+++ Teste/src/util/CategoriaVO.java
@@ -0,0 +1,31 @@
+package util;
+
+/**
+ * 
+ * @author Vinicius
+ *
+ */
+public class CategoriaVO {
+	private long id;
+	private String nome;
+	private String icone;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIcone() {
+		return icone;
+	}
+	public void setIcone(String icone) {
+		this.icone = icone;
+	}
+}
