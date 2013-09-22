e845aeb6810b4e602589da0f6b2671b724588476
Antonio <toni.adlf@hotmail.com>
2013-09-22 17:13:56 -0300

    Tela de busca por amigos

---
+++ MyPlaces/src/br/mackenzie/myplaces/SearchFriendsActivity.java
@@ -0,0 +1,24 @@
+package br.mackenzie.myplaces;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SearchFriendsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_friends_layout);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
